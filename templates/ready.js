60bbda5e19484f3ac74978e1d9171d641cb5bab6
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-03-21 18:51:31 +0100

    Improved js code.

--- templates/ready.js
+++ templates/ready.js
@@ -12,42 +12,42 @@
     }).appendTo("body");
 
     jaxon.command.handler.register("flot.plot", function(args) {
-        var options = args.data.options || {};
-        var graphs = [];
-        var showLabels = false;
+        const options = args.data.options || {};
+        const graphs = [];
+        let showLabels = false;
         args.data.labels = {};
         // Set container size
-        if(args.data.size.width != "")
+        if(args.data.size.width !== "")
         {
             $(args.data.selector).css({width: args.data.size.width});
         }
-        if(args.data.size.height != "")
+        if(args.data.size.height !== "")
         {
             $(args.data.selector).css({height: args.data.size.height});
         }
-        for(var i = 0, ilen = args.data.graphs.length; i < ilen; i++)
+        for(let i = 0, ilen = args.data.graphs.length; i < ilen; i++)
         {
-            var g = args.data.graphs[i];
-            var graph = g.options || {};
+            const g = args.data.graphs[i];
+            const graph = g.options || {};
             graph.data = [];
             if(g.values.data != null)
             {
-                for(var j = 0, jlen = g.points.length; j < jlen; j++)
+                for(let j = 0, jlen = g.points.length; j < jlen; j++)
                 {
-                    var x = g.points[j];
+                    const x = g.points[j];
                     graph.data.push([x, g.values.data[x]]);
                 }
             }
             else if(g.values.func != null)
             {
                 g.values.func = new Function("x", g.values.func);
-                for(var j = 0, jlen = g.points.length; j < jlen; j++)
+                for(let j = 0, jlen = g.points.length; j < jlen; j++)
                 {
-                    var x = g.points[j];
+                    const x = g.points[j];
                     graph.data.push([x, g.values.func(x)]);
                 }
             }
-            if(g.labels.func != null)
+            if(g.labels.func !== null)
             {
                 g.labels.func = new Function("series,x,y", g.labels.func);
             }
@@ -61,21 +61,21 @@
         // Setting ticks
         if(args.data.xaxis.points.length > 0)
         {
-            var ticks = [];
+            const ticks = [];
             if(args.data.xaxis.labels.data != null)
             {
-                for(var i = 0; i < args.data.xaxis.points.length; i++)
+                for(let i = 0; i < args.data.xaxis.points.length; i++)
                 {
-                    var point = args.data.xaxis.points[i];
+                    const point = args.data.xaxis.points[i];
                     ticks.push([point, args.data.xaxis.labels.data[point]]);
                 }
             }
             else if(args.data.xaxis.labels.func != null)
             {
                 args.data.xaxis.labels.func = new Function("x", args.data.xaxis.labels.func);
-                for(var i = 0; i < args.data.xaxis.points.length; i++)
+                for(let i = 0; i < args.data.xaxis.points.length; i++)
                 {
-                    var point = args.data.xaxis.points[i];
+                    const point = args.data.xaxis.points[i];
                     ticks.push([point, args.data.xaxis.labels.func(point)]);
                 }
             }
@@ -96,32 +96,30 @@
         if(showLabels)
         {
             $(args.data.selector).bind("plothover", function (event, pos, item) {
-                if(item)
-                {
-                    var series = item.series.label;
-                    var x = item.datapoint[0]; // item.datapoint[0].toFixed(2);
-                    var y = item.datapoint[1]; // item.datapoint[1].toFixed(2);
-                    var tooltip = "";
-                    if(typeof args.data.labels[series] !== "undefined")
-                    {
-                        var labels = args.data.labels[series];
-                        if(labels.data != null && typeof labels.data[x] !== "undefined")
-                        {
-                            tooltip = labels.data[x];
-                        }
-                        else if(labels.func != null)
-                        {
-                            tooltip = labels.func(series, x, y);
-                        }
-                    }
-                    if((tooltip))
-                    {
-                        $("#flot-tooltip").html(tooltip).css({top: item.pageY+5, left: item.pageX+5}).fadeIn(200);
-                    }
-                }
-                else
+                if(!item)
                 {
                     $("#flot-tooltip").hide();
+                    return;
+                }
+                const series = item.series.label;
+                const x = item.datapoint[0]; // item.datapoint[0].toFixed(2);
+                const y = item.datapoint[1]; // item.datapoint[1].toFixed(2);
+                let tooltip = "";
+                if(typeof args.data.labels[series] !== "undefined")
+                {
+                    const labels = args.data.labels[series];
+                    if(labels.data != null && typeof labels.data[x] !== "undefined")
+                    {
+                        tooltip = labels.data[x];
+                    }
+                    else if(labels.func != null)
+                    {
+                        tooltip = labels.func(series, x, y);
+                    }
+                }
+                if((tooltip))
+                {
+                    $("#flot-tooltip").html(tooltip).css({top: item.pageY+5, left: item.pageX+5}).fadeIn(200);
                 }
             });
         }
